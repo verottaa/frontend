f381131c9317f5942baebe42cd59ea7af023f5ce
KillEmAll <bugurtoff@gmail.com>
2020-09-15 12:15:29 +0300

    Доработка login-page

--- src/app/src/components/login/login.component.ts
+++ src/app/src/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  // constructor(private router: Router,
+  //   private fb: FormBuilder) {
+  //   this.form = fb.group({
+  //   login: fb.control('', [Validators.required]),
+  //   password: fb.control('', [Validators.required]),
+  //   });
+  // }
 
   ngOnInit(): void {
   }
 
+  // onSubmit() {
+  //   if(!this.form.invalid && this.form.touched){
+  //     this.router.navigate(['/']);
+  //   } else {
+  //     this.form.markAllAsTouched();
+  //   }
+  // }
 }
