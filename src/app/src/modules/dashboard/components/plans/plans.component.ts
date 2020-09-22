50f6fcb14efe20314c146f4a90c33d5621ba7245
KillEmAll <bugurtoff@gmail.com>
2020-09-22 14:21:06 +0300

    Хотфикс

--- src/app/src/modules/dashboard/components/plans/plans.component.ts
+++ src/app/src/modules/dashboard/components/plans/plans.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-plans',
@@ -10,7 +10,17 @@ export class PlansComponent implements OnInit {
   plansForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.plansForm = fb.group({
-      
+      empl: fb.control('', [Validators.required]),
+      direction: fb.control('', [Validators.required]),
+      periodOfStudy: fb.control('', [Validators.required]),
+      mentor: fb.control('', [Validators.required]),
+      projectTask: fb.control('', [Validators.required]),
+      conducting: fb.control('', [Validators.required]),
+      peridoPlan: fb.control('', [Validators.required]),
+      peridoFact: fb.control('', [Validators.required]),
+      curator: fb.control('', [Validators.required]),
+      materialLink: fb.control('', [Validators.required]),
+      note: fb.control('', [Validators.required])
     });
    }
 
