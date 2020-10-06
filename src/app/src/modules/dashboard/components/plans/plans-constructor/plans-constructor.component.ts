13e55a832ad7a5f0db791e93f9632405cf433e9c
urglmorl <eriksongerson@yandex.ru>
2020-10-06 23:10:11 +0300

    Plans constructor

--- src/app/src/modules/dashboard/components/plans/plans-constructor/plans-constructor.component.ts
+++ src/app/src/modules/dashboard/components/plans/plans-constructor/plans-constructor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-plans-constructor',
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlansConstructorComponent implements OnInit {
 
-  constructor() { }
+  templateStep: Template[] = [
+    {
+      task: 'Документация',
+      formLearn: 'Самостоятельное изучение',
+      doPeriod: '1 день',
+      dateStart: '01.01.2020',
+      dateEnd: '02.02.2020',
+      mentor: 'Нижний Новгород',
+      materials: 'http://link'
+    },
+    {
+      task: 'Документация',
+      formLearn: 'Самостоятельное изучение',
+      doPeriod: '1 день',
+      dateStart: '01.01.2020',
+      dateEnd: '02.02.2020',
+      mentor: 'Нижний Новгород',
+      materials: 'http://link'
+    },
+    {
+      task: 'Документация',
+      formLearn: 'Самостоятельное изучение',
+      doPeriod: '1 день',
+      dateStart: '01.01.2020',
+      dateEnd: '02.02.2020',
+      mentor: 'Нижний Новгород',
+      materials: 'http://link'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface Template {
+  task: string;
+  formLearn: string;
+  doPeriod: string;
+  dateStart: string;
+  dateEnd: string;
+  mentor: string;
+  materials: string;
 }
